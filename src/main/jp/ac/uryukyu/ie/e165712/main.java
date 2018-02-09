1cc6ad26694379a55b6b458600bc0c22d37f997c
Keisuke Yasuda <e165712@ie.u-ryukyu.ac.jp>
2018-02-09 16:40:49 +0900

    2

--- src/main/jp/ac/uryukyu/ie/e165712/main.java
+++ src/main/jp/ac/uryukyu/ie/e165712/main.java
@@ -3,6 +3,12 @@ package jp.ac.uryukyu.ie.e165712;
 public class main {
     public static void main(String[] args){
         String str = null;
-        System.out.printf("%d\n", str.length());
+        try {
+            System.out.printf("%d\n", str.length());
+        }
+        catch(NullPointerException e){
+            System.out.printf("例外: NullPointerException が発生しました。\n");
+            System.out.printf("%s\n",e.getMessage());
+        }
     }
 }
