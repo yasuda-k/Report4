a0f92d553b9392a81022aad95410638e5cb8ba7f
Keisuke Yasuda <e165712@ie.u-ryukyu.ac.jp>
2018-02-09 15:28:40 +0900

    1st

---
+++ src/main/jp/ac/uryukyu/ie/e165712/main.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e165712;
+
+public class main {
+    public static void main(String[] args){
+        String str = null;
+        System.out.printf("%d\n", str.length());
+    }
+}
